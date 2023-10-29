4fe11923816d46147c28d4dccf343b9e60078703
BAKHTIYOR-RAKHMATULLAYEV <148712650+BAKHTIYOR-RAKHMATULLAYEV@users.noreply.github.com>
2023-10-29 16:52:23 +0500

    Update problem_5.cpp

--- problem_5.cpp
+++ problem_5.cpp
@@ -1,23 +1,15 @@
 #include <iostream>
- using namespace std;
- 
- int main (){
-     int h;
-     cin >> h;
-     int start = h-1,counter = 1;
-     for(int i = 1;i < h; i++){
-         for(int j = 1;j < start; j++){
-             cout << " ";
-         }
-        
-         for(int j=0; j < counter; j++){
-             cout << "*";
-         } 
-          cout << endl;
-          start--;
-         counter+=2;
-        
-     }
-     
-     return 0;
- }
+using namespace std;
+int main () {
+    int i,j,n;
+    cin>>n;
+    for(i=0; i<n; i++)
+{
+    for(j=1; j<=n-i;j++)
+        cout<<" ";
+    for(j=1;j<=2*i-1;j++)
+        cout<<"*";
+    cout<<'\n';
+}
+    return 0;
+}
